52952fbb1ae290b1632f6f8744599eb435e6049a
“hu-snail” <1217437592@qq.com>
2021-10-25 10:34:10 +0800

    代码完善

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# vite-plguin-pdf2vue
+# vite-plugin-markdown2vue
+
+## 说明
 Convert PDF to vue3 component
+
+## 特性
+- 默认支持[highlight.js](https://fenxianglu.cn/highlight.html)主题
+- 默认markdown主题为gitbook
+  - 主题一：gitbook-azure
+  - 主题二：gitbook-slate
+  - 主题三：gitbook-teal  
+
+## 安装
+```sh
+npm i vite-plugin-markdown2vue --save-dev
+
+yarn add vite-plugin-markdown2vue -D
+```
+
+## 实用
+```js
+// vite.config.js
+import { defineConfig } from "vite";
+import vitePluginMd2Vue from "vite-plugin-markdown2vue";
+
+export default defineConfig({
+  ...
+  plugins: [vitePluginMd2Vue({codeTheme: 'github', theme: 'gitbook-teal'})]
+});
+```
+
+## vue3 Example
+```vue
+<template>
+  <Test />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Test, { headings }  from 'docs/test.md'
+
+export default defineComponent({
+  name: 'Test',
+  components: {
+    Test
+  },
+})
+</script>
+```
+
+
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const marked = require("marked");
+
+import hljs from "highlight.js";
+
+let defaultRenderer = {};
+let headerList = []
+export default function ({codeTheme = 'github-dark', theme = 'gitbook-teal'}) {
+  return {
+    name: "vitePluginMd2Vue",
+    transform(src, id) {
+      if (id.endsWith(".md")) {
+        marked.setOptions({
+          highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+          },
+        });
+
+        defaultRenderer = {
+          heading: function (text, level) {
+            headerList.push({text, level})
+            const escapedText = text.toLowerCase().replace(/[^\w]+/g, "-");
+            return `
+              <h${level}>
+                <a name="${escapedText}" class="anchor" href="#${escapedText}">
+                  <span class="header-link"></span>
+                </a>
+                ${text}
+              </h${level}>`;
+          },
+        };
+
+        marked.use({
+          renderer: defaultRenderer,
+        });
+
+        return {
+          code: `import {h, defineComponent} from "vue";
+                 import 'highlight.js/styles/${codeTheme}.css'
+                 import './theme/${theme}.css'
+                const _sfc_md = defineComponent({
+                    name: "Markdown",
+                });
+
+                const _sfc_render =() => {
+                    return h("div", {
+                      innerHTML: ${JSON.stringify(marked(src))}, 
+                    })
+                };
+
+                _sfc_md.render = _sfc_render
+                export default _sfc_md
+                export const headings = ${JSON.stringify(headerList)}`,
+          map: null,
+        };
+      }
+    },
+  };
+}
